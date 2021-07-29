3ea74626e9ca7a11d4d04bff6af9e2ec71ed1039
Piotr Ksiazek <pksiazek.py@gmail.com>
2021-07-29 16:45:22 +0200

    Clean up constants and add docstring to TranslateMatrixUnits

--- Assets/Scripts/ChessPieces/PieceFactory.cs
+++ Assets/Scripts/ChessPieces/PieceFactory.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 using System;
 public class PieceFactory : MonoBehaviour
 {
+    private const float _upperLeftCornerX = -3.5f;
+    private const float _upperRightCornerY = -_upperLeftCornerX;
+
     private const int _z = -1;
     public static GameObject Create(GameObject go, int x, int y)
     {
@@ -11,10 +14,24 @@ public class PieceFactory : MonoBehaviour
         return Instantiate(go, translatedUnits, Quaternion.identity);
     }
 
+    /// <summary>
+    /// On the game matrix pieces will be represented as follows
+    ///[0, 0][1, 0][2, 0][3, 0][4, 0][5, 0][6, 0][7, 0]
+    ///[0, 1][1, 1][2, 1][3, 1][4, 1][5, 1][6, 1][7, 1]
+    ///[0, 2][1, 2][2, 2][3, 2][4, 2][5, 2][6, 2][7, 2]
+    ///[0, 3][1, 3][2, 3][3, 3][4, 3][5, 3][6, 3][7, 3]
+    ///[0, 4][1, 4][2, 4][3, 4][4, 4][5, 4][6, 4][7, 4]
+    ///[0, 5][1, 5][2, 5][3, 5][4, 5][5, 5][6, 5][7, 5]
+    ///[0, 6][1, 6][2, 6][3, 6][4, 6][5, 6][6, 6][7, 6]
+    ///[0, 7][1, 7][2, 7][3, 7][4, 7][5, 7][6, 7][7, 7]
+    /// </summary>
+    /// <param name="matrixX">X coordinate starting from upper left corner</param>
+    /// <param name="matrixY">Y coordinate starting from upper left corner</param>
+    /// <returns></returns>
     public static Vector3 TranslateMatrixUnitsToWorldUnits(int matrixX, int matrixY)
     {
-        float worldX = -3.5f + matrixX;
-        float worldY = 3.5f - matrixY;
+        float worldX = _upperLeftCornerX + matrixX;
+        float worldY = _upperRightCornerY - matrixY;
         return new Vector3(worldX, worldY, _z);
     }
 }
